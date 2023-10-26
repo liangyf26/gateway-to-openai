d449dbad171d20e2093514094c81206335f7197b
peachimmortal6 <76577743@qq.com>
2023-10-26 14:41:04 +0800

    Update _worker.js
    add logs

--- _worker.js
+++ _worker.js
@@ -1,7 +1,9 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
+    console.log(url);
     url.host = "api.openai.com";
+    console.log(url);
     // openai is already set all CORS heasders 
     return fetch(url, {
       headers: request.headers,
